a479a25b4ec171e1bd3f3f3bc5edd65dd4aca85c
Daniel Jensen <danto7@users.noreply.github.com>
2022-10-31 11:39:21 +0100

    sponsorblock do not remove

--- ytdlp.rb
+++ ytdlp.rb
@@ -1,7 +1,7 @@
-SPONSORBLOCK = "--force-keyframes-at-cuts --sponsorblock-remove sponsor --sponsorblock-mark default --embed-thumbnail"
+OPTIONS = "--sponsorblock-mark default --embed-thumbnail --recode-video mkv"
 
 class Ytdlp
   def download(url, out_dir)
-    system "yt-dlp --output '#{out_dir}/%(title)s.%(ext)s' #{SPONSORBLOCK} '#{url}'"
+    system "yt-dlp --output '#{out_dir}/%(title)s.%(ext)s' #{OPTIONS} '#{url}'"
   end
 end
